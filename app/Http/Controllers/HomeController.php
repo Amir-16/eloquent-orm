6bc37d453276abf5e59a4e89610da96420b00c36
Amir-16 <greenfitter516@gmail.com>
2021-01-09 10:23:02 +0600

    one to one

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
+use App\Model\Nid;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    // public function getInfo(){
+    //
+    //     $user=User::find(Auth::user()->id);
+    //     return view('welcome',compact('user'));
+    // }
+
+    //if we want data from Nid
+
+    public function getInfo(){
+      $nid =Nid::where('user_id',Auth::user()->id)->first();
+      return view('welcome',compact('nid'));
+    }
+
+}
